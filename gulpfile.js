0704eb2341d25982a32b2cabc1abfec7ad592484
jacob <jacob@outerplaces.com>
2016-10-16 10:02:46 -0400

    second commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+
+
+gulp.task('js', function(){
+	browserify('vdl.js')
+		.bundle()
+		.on('error', function(e){
+			gutil.log(e);
+		})
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('js'))
+});
+
+//default task
+gulp.task('default',function() {
+    gulp.watch('vdl.js',['js']);
+});
